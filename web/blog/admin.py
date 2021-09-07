54f37d8b7ca4a89c218bf4681c92785cf54b6dc1
NhoangPhung0809 <nphung518@gmail.com>
2021-09-07 21:18:48 +0700

    admin

---
+++ web/blog/admin.py
@@ -0,0 +1,10 @@
+from typing import List
+from django.contrib import admin
+from .models import Post
+
+# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'date']
+    list_filter = ['date']
+    search_fields = ['title']
+admin.site.register(Post, PostAdmin)
--- web/home/urls.py
+++ web/home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.urls.conf import include
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('contact/', views.contact)
 ]
 
